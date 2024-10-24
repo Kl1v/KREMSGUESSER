8e279248513837c6a0e5ad268ad205bf218736ac
Laurenz Pichler <l.pichler@htlkrems.at>
2024-10-24 14:04:47 +0200

    Test

--- connection.php
+++ connection.php
@@ -1,12 +1,17 @@
 <?php
 $servername = "medtdb.mysql.database.azure.com";
 $username = "kliv@medtdb";
-$password = "Antonginger1";
+$password = "Antonginger1"; 
 $dbname = "opendays";
 
+$ssl_ca = "C:\\Users\\Laurenz\\Downloads\\DigiCertGlobalRootG2.crt.pem";
+
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password, [
+        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
+    ]);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Verbindung erfolgreich!";
 } catch (PDOException $e) {
     die("Verbindung fehlgeschlagen: " . $e->getMessage());
 }
