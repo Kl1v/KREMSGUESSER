5f29a4379cbb41e152c40ddf32ec5e71bcc7a917
Laurenz Pichler <l.pichler@htlkrems.at>
2024-10-24 15:10:00 +0200

    Test

--- scoreboard.php
+++ scoreboard.php
@@ -2,17 +2,23 @@
 
 include 'connection.php'; 
 
-$sql = "SELECT name, score FROM scoreboard ORDER BY score DESC";
-$result = $conn->query($sql);
+// Abrufen der Punktestände
+function fetchScores($conn) {
+    $sql = "SELECT name, score FROM scoreboard ORDER BY score DESC";
+    $result = $conn->query($sql);
 
-$scores = array();
+    $scores = array();
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $scores[] = $row;
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $scores[] = $row;
+        }
     }
+
+    return $scores;
 }
 
+$scores = fetchScores($conn);
 $conn->close();
 ?>
 
@@ -42,7 +48,7 @@ $conn->close();
 <body>
     <div class="container">
         <h1 class="text-center">Scoreboard</h1>
-        <table class="table table-striped">
+        <table class="table table-striped" id="scoreTable">
             <thead>
                 <tr>
                     <th scope="col">Platz</th>
@@ -63,6 +69,40 @@ $conn->close();
             </tbody>
         </table>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+    function fetchScores() {
+        $.ajax({
+            url: '', // aktuelle Datei
+            type: 'POST',
+            data: {
+                fetch: true
+            },
+            success: function(data) {
+                const scores = JSON.parse(data);
+                updateTable(scores);
+            }
+        });
+    }
+
+    function updateTable(scores) {
+        const tableBody = $('#scoreTable tbody');
+        tableBody.empty(); // Bestehende Zeilen leeren
+
+        scores.forEach((player, index) => {
+            tableBody.append(`
+                    <tr>
+                        <th scope='row'>${index + 1}</th>
+                        <td>${player.name}</td>
+                        <td>${player.score}</td>
+                    </tr>
+                `);
+        });
+    }
+
+    setInterval(fetchScores, 1000);
+    </script>
 </body>
 
 </html>
