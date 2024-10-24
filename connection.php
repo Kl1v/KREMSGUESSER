cd0d0da28b931f2280405ee6d0cdc5fbaffcb4db
Laurenz Pichler <l.pichler@htlkrems.at>
2024-10-24 14:55:38 +0200

    Test

--- connection.php
+++ connection.php
@@ -1,18 +1,22 @@
 <?php
 $servername = "medtdb.mysql.database.azure.com";
-$username = "kliv@medtdb";
-$password = "xxx"; 
+$username = "kliv";
+$password = "Antonginger1"; // Ersetze xxx mit deinem Passwort
 $dbname = "opendays";
 
-$ssl_ca = "\DigiCertGlobalRootG2.crt.pem";
+// Pfad zum SSL-Zertifikat
+$ssl_ca = "C:\\xampp\\htdocs\\KREMSGUESSER\\DigiCertGlobalRootG2.crt.pem";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password, [
-        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
-    ]);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Verbindung erfolgreich!";
-} catch (PDOException $e) {
-    die("Verbindung fehlgeschlagens: " . $e->getMessage());
+// Initialisiere MySQLi
+$conn = mysqli_init();
+
+// SSL-Einstellungen setzen
+mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
+
+// Verbinde ohne Zertifikatsprüfung
+if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL | MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
+    die("Verbindung fehlgeschlagen: " . mysqli_connect_error());
 }
+
+echo "Verbindung erfolgreich!";
 ?>
